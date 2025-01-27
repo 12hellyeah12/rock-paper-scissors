11f74c44ad952e152b10d8e5b669d7615ab2c162
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-01-27 20:58:44 +0100

    Write logic to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Declare score variables
+let humanScore = 0;
+let computerScore = 0;
+
 // Create function getComputerChoice
 const getComputerChoice = () => {
     // Initalize 1 variable "computerChoice"
@@ -20,10 +24,58 @@ const getComputerChoice = () => {
         computerChoice = "Paper";
     }
     // Return "computerChoice"
-    console.log(computerChoice);
+    return computerChoice;
 }
 
 // Create a function called getHumanChoice
 // Prompt the user for input and assign it to userChoice
 // Return userChoice
 const getHumanChoice = () => userChoice = window.prompt("Rock, Paper or Scissor?", "e.g. Rock, Paper or Scissor");
+
+// Create a functionn called playRound with two parameters "computerChoice" and "userChoice"
+// Make the arguments case insensitive
+const playRound = (computer, user) => {
+    // Make arguments case insensitive
+    const computerChoice = computer.toLowerCase();
+    const userChoice = user.toLowerCase();
+    // Define winner variable
+    let computerWins = false;
+    let isTie = false;
+    // Define cases for computer to win
+    if(computerChoice === "rock" && userChoice === "scissor"){
+        computerWins = true;
+    } else if(computerChoice === "paper" && userChoice === "rock"){
+        computerWins = true
+    } else if(computerChoice === "scissor" && userChoice === "paper"){
+        computerWins = true;
+    } else if(computerChoice === userChoice){
+        isTie = true;
+    }
+    else {
+        computerWins = false;
+    }
+
+    // If "computerChoice" beats "userChoice" 
+    if(computerWins){
+        // Then display result in the console 
+        console.log(`You loose! ${computerChoice} beats ${userChoice}`);
+         // Add one point to the computers score and display the scores in the console
+         computerScore += 1;
+    } else if(isTie){
+        console.log(`It is a ${computerChoice}-tie!`)
+    } else {
+        console.log(`You win! ${userChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } 
+    console.log(`Computer Score: ${computerScore} User Score: ${humanScore}`);
+}
+
+playRound(getComputerChoice(), getHumanChoice());
+
+    
+
+    
+    
+        
+       
+
